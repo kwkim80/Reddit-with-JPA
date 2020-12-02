81b6694a041f288549b3e91f3317aff3425546a9
adam <adam@10.211.55.4>
2020-12-02 14:37:16 -0500

    commited

--- src/main/java/dal/CommentDAL.java
+++ src/main/java/dal/CommentDAL.java
@@ -5,10 +5,47 @@
  */
 package dal;
 
+import entity.Comment;
+import java.sql.Date;
+import java.util.List;
+
 /**
  *
  * @author kw244
  */
 public class CommentDAL {
+    public CommentDAL(){
+        
+    }
+    public List<Comment> findAll(){
+            return null;
+    }
+    public Comment findById(int id){
+        return null;
+    }
+    
+    public Comment findByUniqueId(String uniqueID){
+        return null;
+    }
+    
+    public List<Comment> findByText(String text){
+        return null;
+    }
+    
+    public List<Comment> findByCreated(Date created){
+        return null;
+    }
+    
+        public List<Comment> findPoints(int points){
+        return null;
+    }
+            public List<Comment> findByReplys(int replys){
+        return null;
+    }
+                public List<Comment> findByIsReply(boolean isReply){
+        return null;
+    }
+                
+    
     
 }
