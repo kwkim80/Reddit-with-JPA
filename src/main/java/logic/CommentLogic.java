f3c21d4f885bf14fa49c08b90ee5e3c5f07577cd
adam <adam@10.211.55.4>
2020-12-03 21:49:14 -0500

    commited

--- src/main/java/logic/CommentLogic.java
+++ src/main/java/logic/CommentLogic.java
@@ -26,10 +26,7 @@ import static logic.AccountLogic.ID;
 import static logic.AccountLogic.PASSWORD;
 import static logic.AccountLogic.USERNAME;
 
-/**
- *
- * @author kw244
- */
+
 public class CommentLogic extends GenericLogic<Comment, CommentDAL> {
 public final String REPLYS = "replys";
 public final String IS_REPLY = "is_reply";
@@ -40,7 +37,6 @@ public final String ID = "id";
 public final String UNIQUE_ID = "unique_id";
 public final String REDDIT_ACCOUNT_ID = "reddit_account_id";
 public final String POST_ID = "post_id";
-
 
 private  CommentLogic(){
     super(new CommentDAL());
@@ -82,17 +78,9 @@ public List<Comment> getCommentsWithReplys(boolean isReply){
 public Comment createEntity(Map<String, String[]> parameterMap){
  
         Objects.requireNonNull( parameterMap, "parameterMap cannot be null" );
-        //same as if condition below
-//        if (parameterMap == null) {
-//            throw new NullPointerException("parameterMap cannot be null");
-//        }
 
-        //create a new Entity object
         Comment entity = new Comment();
 
-        //ID is generated, so if it exists add it to the entity object
-        //otherwise it does not matter as mysql will create an if for it.
-        //the only time that we will have id is for update behaviour.
         if( parameterMap.containsKey( ID ) ){
             try {
                 entity.setId( Integer.parseInt( parameterMap.get( ID )[ 0 ] ) );
@@ -101,9 +89,6 @@ public Comment createEntity(Map<String, String[]> parameterMap){
             }
         }
 
-        //before using the values in the map, make sure to do error checking.
-        //simple lambda to validate a string, this can also be place in another
-        //method to be shared amoung all logic classes.
         ObjIntConsumer< String> validator = ( value, length ) -> {
             if( value == null || value.trim().isEmpty() || value.length() > length ){
                 String error = "";
@@ -117,11 +102,6 @@ public Comment createEntity(Map<String, String[]> parameterMap){
             }
         };
 
-        //extract the date from map first.
-        //everything in the parameterMap is string so it must first be
-        //converted to appropriate type. have in mind that values are
-        //stored in an array of String; almost always the value is at
-        //index zero unless you have used duplicated key/name somewhere.
         String reddit_account_id = parameterMap.get(REDDIT_ACCOUNT_ID )[ 0 ];
         String post_id = parameterMap.get( POST_ID )[ 0 ];
         String unique_id = parameterMap.get( UNIQUE_ID )[ 0 ];
@@ -131,51 +111,50 @@ public Comment createEntity(Map<String, String[]> parameterMap){
         String replys = parameterMap.get( REPLYS )[ 0 ];
         String is_reply = parameterMap.get( IS_REPLY )[ 0 ];
 
-      
-        //validate the data
         if (isNumeric(reddit_account_id)!=true){
-           throw new ValidationException( "non numeric" );
+            throw new ValidationException( "non numeric" );
         }
-            if (isNumeric(post_id)!=true){
-           throw new ValidationException( "non numeric" );
+        if (isNumeric(post_id)!=true){
+            throw new ValidationException( "non numeric" );
         }
         validator.accept( unique_id, 10 );
         validator.accept( text, 1000 );
-      //  validator.accept( created, 45 );
-   if (isNumeric(points)!=true){
+
+        if (isNumeric(points)!=true){
            throw new ValidationException( "non numeric" );
         }
             if (isNumeric(replys)!=true){
            throw new ValidationException( "non numeric" );
         }
         validator.accept( is_reply, 1 );
-        //set values on entity
+
           RedditAccount reddId = new RedditAccount(Integer.valueOf(reddit_account_id));
           Post postId = new Post(Integer.valueOf(post_id));
-    try { 
-        Date date1=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(created);
-        entity.setCreated(date1);
-    } catch (ParseException ex) {
-        throw new ValidationException("Invalid date");
-    }
+          
+        entity.setCreated(convertStringToDate(created));
+        
         entity.setRedditAccountId(reddId);
         entity.setPostId(postId );
         entity.setUniqueId(unique_id);
+        entity.setPoints(Integer.valueOf(points));
+        entity.setReplys(Integer.valueOf(replys));
+        entity.setIsReply(Boolean.getBoolean(is_reply));
+        
         
 
         return entity;
 }
 
 public List<String> getColumnNames(){
-    return null;
+    return Arrays.asList("id","reddit_account_id","post_id","unique_id","text","created","points","replys","is_reply");
 }
 
 public List<String> getColumnCodes(){
-    return null;
+    return Arrays.asList(ID,REDDIT_ACCOUNT_ID,POST_ID,UNIQUE_ID,TEXT,CREATED,POINTS,IS_REPLY);
 }
 
-public List<?> extractDataAsList(Comment e){
-    return null;
+public List<?> extractDataAsList(Comment c){
+    return Arrays.asList(c.getId(),c.getRedditAccountId(),c.getPostId(),c.getUniqueId(),c.getText(),c.getCreated(),c.getPoints(),c.getIsReply());
 }
 public static boolean isNumeric(String strNum) {
     if (strNum == null) {
